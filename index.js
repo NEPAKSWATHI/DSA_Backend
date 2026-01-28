2c23b300d41e2dcb4c17ad5f6ddd7b911ce04466
SwathiNepak <swathi14714@gmail.com>
2026-01-28 19:02:55 +0530

    added the port in index.js file

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// const authRoutes = require("./routes/auth");
-// app.use("/api/auth", authRoutes);
-
-// const problemRoutes = require("./routes/problems");
-// app.use("/api/problems", problemRoutes);
-    
-// const progressRoutes = require("./routes/progress");
-// app.use("/api/progress", progressRoutes);
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch(err => console.log("Mongo error:", err));
-
-// app.get("/", (req, res) => {
-//   res.send("DSA Tracker Backend Running 🚀");
-// });
-
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -41,7 +11,17 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/problems", require("./routes/problems"));
 app.use("/api/progress", require("./routes/progress"));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB connected"));
 
-app.listen(5000, () => console.log("Server running"));
+// app.listen(5000, () => console.log("Server running"));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
